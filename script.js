1a09c816909efb5a974cd8445f037700bd5acddd
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2020-12-10 00:08:30 +0300

    first comit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+let age = { '23' : 22};
+
+
+
+let objFunc = {
+
+  'age': 'возраст',
+  'name': 'имя',
+  'fuc': ()=> {
+    console.log(age);
+    console.log(this.name);
+  }
+}
+objFunc.fuc();
+
+// objFunc.fuc.bind('ss');
+
+objFunc.fuc();
