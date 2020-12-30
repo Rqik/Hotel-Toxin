08090c93a85370b000e1ec2e9760a21364bc64ae
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2020-12-30 10:58:49 +0300

    datepicker v0.2 final

--- src/pug/components/data-select/data-select.js
+++ src/pug/components/data-select/data-select.js
@@ -1,40 +1,56 @@
 import 'air-datepicker'
-$.fn.datepicker.language['ru'] =  {
-  days: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
-  daysShort: ['Вос','Пон','Вто','Сре','Чет','Пят','Суб'],
-  daysMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-  months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-  monthsShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
-  today: 'Применить',
-  clear: 'Очистить',
-  dateFormat: 'dd.mm.yyyy',
-  timeFormat: 'hh:ii',
-  firstDay: 1
-};
-
+import 'jquery-mask-plugin'
 let props = {
   position: "bottom center",
   range: true,
   navTitles: {
     days: 'MM <br>yyyy',
   },
+  language: {
+    today: 'Применить',
+  },
   clearButton: true,
   prevHtml: '<i class="material-icons">arrow_back</i>',
   nextHtml: '<i class="material-icons">arrow_forward</i>',
   todayButton: true,
-}
-$('.data__select').datepicker(props)
-
-function swapButton(selector) {
-  $(selector).html(function (i, el) {
-    console.log($(this).data('action'))
-    if ($(this).data('action') == 'prev') {
-      return '<i class="material-icons">arrow_back</i>'
-    }
-    return '<i class="material-icons">arrow_forward</i>'
-  })
-
+  minDate: new Date(),
+  keyboardNav: false,
+  multipleDates: 2
 }
 
-// let start = $('.data__select_input-left').datepicker(props).data('datepicker')
-// let end = $('.data__select_input-right').datepicker(props).data('datepicker')
+let center = $('.date_picker').datepicker(props)
+center.data('datepicker').hide()
+$('.data__select_label').click(function () {
+  $(this).siblings(".date_picker").data('datepicker').show()
+})
+
+// попытка реализовать изменение в datepicker через инпут
+let pervDay =  new Date()
+let nexDay  =  new Date()
+$(".data__select_input-left").change(function () {
+  let ss = $(this).closest('.data__select_label').siblings(".date_picker").data('datepicker')
+  let date = $(this).val().split('.').reverse()
+  date[1] = +date[1] - 1
+  pervDay = new Date(...date) > new Date()? new Date(...date):new Date()
+  ss.selectDate([pervDay, nexDay])
+})
+$(".data__select_input-right").change(function () {
+  let ss = $(this).closest('.data__select_label').siblings(".date_picker").data('datepicker')
+  let date = $(this).val().split('.').reverse()
+  date[1] = +date[1] - 1
+  nexDay = new Date(...date) > new Date()? new Date(...date):new Date()
+  ss.selectDate([pervDay, nexDay])
+})
+
+
+
+center.datepicker({
+  onSelect: function (fd, date, picker) {
+    let el = picker.$el.siblings('.data__select_label')
+    console.log(fd);
+    el.children(".data__select_input-left").val(fd.split(",")[0]);
+    el.children(".data__select_input-right").val(fd.split(",")[1]);
+  }
+})
+
+$('.data__select_input').mask('00.00.0000')
