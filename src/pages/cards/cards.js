2f10814935d49cd97b146caf1a95f4f8c9166c0e
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2021-04-01 23:58:22 +0300

    cards page , fix styles

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -5,3 +5,25 @@ import '@/components/card-room';
 import '@/components/card-auth';
 import '@/components/card-registration';
 import '@/components/field';
+
+const myDatepicker = $('.calendar').datepicker({
+  position: 'bottom center',
+  range: true,
+  navTitles: {
+    days: 'MM <br>yyyy',
+  },
+  clearButton: true,
+  prevHtml: '<i class="material-icons">arrow_back</i>',
+  nextHtml: '<i class="material-icons">arrow_forward</i>',
+  minDate: new Date(),
+  keyboardNav: false,
+  multipleDates: 2,
+  onSelect(fd, date, picker) {
+    const el = picker.$el.siblings('.js-data-select__label');
+    el.children('.js-data-select__input_left')
+      .val(fd.split(',')[0]);
+    el.children('.js-data-select__input_right')
+      .val(fd.split(',')[1]);
+  },
+}).data('datepicker');
+myDatepicker.show();
