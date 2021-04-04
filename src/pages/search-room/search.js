ee664a137cd229687b0d9911c0a9030a3d47c48a
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2021-04-04 21:32:25 +0300

    fix tabs

--- src/pages/search-room/search.js
+++ src/pages/search-room/search.js
@@ -1,5 +1,7 @@
 $(() => {
-  $('.js-search-page__panel-btn').on('click', () => {
-    $('.js-search-page__panel').toggleClass('search-page__panel_fixed');
-  });
+  $('.js-search-page__panel-btn')
+    .on('click', () => {
+      $('.js-search-page__panel')
+        .toggleClass('search-page__panel_fixed');
+    });
 });
