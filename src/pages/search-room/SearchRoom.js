bb4fd11825f26c7dcfda685eead22f77326f3dbb
Arsen Gadjiusmanov <arsen.gadjiusmanov@mail.ru>
2022-10-05 05:15:33 +0300

    fix bind

--- src/pages/search-room/SearchRoom.js
+++ src/pages/search-room/SearchRoom.js
@@ -1,19 +1,23 @@
 class SearchRoom {
+  $searchPanel = null;
+
+  $pageButton = null;
+
   constructor() {
     this.findSearchPageElement();
   }
 
   findSearchPageElement() {
-    this.pageButton = $('.js-search-page__panel-btn');
-    this.searchPanel = $('.js-search-page__panel');
+    this.$pageButton = $('.js-search-page__panel-btn');
+    this.$searchPanel = $('.js-search-page__panel');
   }
 
   init() {
-    this.pageButton.on('click', SearchRoom.onClickSearchPanel);
+    this.$pageButton.on('click', SearchRoom.onClickSearchPanel.bind(this));
   }
 
   static onClickSearchPanel() {
-    this.searchPanel.toggleClass('search-page__panel_fixed');
+    this.$searchPanel.toggleClass('search-page__panel_fixed');
   }
 }
 
